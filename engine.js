9cf2a7a4ad1c41efa49e61901fbf1d4b4faaa65b
john525 <john.lhota.525@gmail.com>
2014-04-20 22:49:35 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -202,13 +202,11 @@ function updateGame() {
         ctx.fillRect(0,0,LEN*SIZE+(LEN+1)*BORDER,LEN*SIZE+(LEN+1)*BORDER);
         ctx.fillStyle = "#FFF";
         
-        ctx.
-        
         ctx.font = "bold 36pt Arial";
-        ctx.fillText("Game Over", LEN*SIZE/2-w/2, LEN*SIZE/3);
+        ctx.fillText("Game Over", (LEN*SIZE + (LEN+1)*BORDER)/2, LEN*SIZE/3);
         
         ctx.font = "18pt Arial";
-        ctx.fillText("Click to Restart", LEN*SIZE/2, LEN*SIZE/2);
+        ctx.fillText("Click to Restart", (LEN*SIZE + (LEN+1)*BORDER)/2, LEN*SIZE/2);
         return;
     }
 }
